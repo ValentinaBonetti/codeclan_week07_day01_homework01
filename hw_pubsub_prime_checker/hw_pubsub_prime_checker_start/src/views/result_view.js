f5e287c4fc6137f8d2d828be6307aeec6ee0baa5
Valentina Bonetti <valentina.bonetti@strath.ac.uk>
2018-10-15 17:10:44 +0100

    Result view receives the result and displays it in the browser

--- hw_pubsub_prime_checker/hw_pubsub_prime_checker_start/src/views/result_view.js
+++ hw_pubsub_prime_checker/hw_pubsub_prime_checker_start/src/views/result_view.js
@@ -5,8 +5,21 @@ const ResultView = function(){
 };
 
 ResultView.prototype.bindEvents = function () {
-  PubSub.subscribe('PrimeChecker:result-calculated');
+  PubSub.subscribe('PrimeChecker:result-calculated',(event) => {
+    const isPrimeTrueOrFalse = event.detail;
+    this.displayResult(isPrimeTrueOrFalse);
+  });
+
 };
 
+
+ResultView.prototype.displayResult = function (result) {
+  const resultElement = document.querySelector('#result');
+  if (result === true){
+    resultElement.textContent = `Yes! It's a prime number.`
+  }else{
+    resultElement.textContent = `Sadly not, it's not prime.`
+  }
+};
 
 module.exports = ResultView;
